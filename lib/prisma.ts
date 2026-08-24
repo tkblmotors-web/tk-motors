a4da83049626fd2333b6cbaa07f79c06f4dd8a4b
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-24 17:50:38 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ connectionString: process.env.POSTGRES_PRISMA_URL });
