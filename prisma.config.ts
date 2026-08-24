d86719b52b0e41029d88e87b949b504d1e1cf587
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-24 16:21:17 +0100

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,8 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+  schema: 'prisma/schema.prisma',
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.POSTGRES_PRISMA_URL,
   },
 });
