b5700c992205be2bfa0050417e3734fbc615f5df
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-25 00:55:06 +0100

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from '@prisma/config';
-import 'dotenv/config';
+import { defineConfig } from "prisma/config";
+import path from "node:path";
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: path.join("prisma", "schema.prisma"),
+  datasource: {
+    url: process.env.POSTGRES_PRISMA_URL,
+    directUrl: process.env.POSTGRES_URL_NON_POOLING,
+  },
 });
