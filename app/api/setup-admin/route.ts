55f750a8a4df340b3435656e0ad3eaa2c0022963
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-25 19:35:45 +0100

    Update route.ts

--- app/api/setup-admin/route.ts
+++ app/api/setup-admin/route.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
