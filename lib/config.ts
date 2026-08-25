45ec15964190c80912b354b028862b869bce0e2d
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-25 15:22:30 +0100

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
   name: "TK MOTORS",
   tagline: "Korean vehicle imports, inspected and delivered to Algeria",
-  whatsappNumber: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || "213000000000",
+  whatsappNumber: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || "213558509365",
   phoneDisplay: process.env.NEXT_PUBLIC_PHONE_DISPLAY || "+213 00 00 00 00",
-  email: process.env.NEXT_PUBLIC_CONTACT_EMAIL || "contact@tkmotors.dz",
+  email: process.env.NEXT_PUBLIC_CONTACT_EMAIL || "tkbl.motors@gmail.com",
   city: "Algiers, Algeria",
-  originPort: "Busan, South Korea",
-  destinationPort: "Port of Algiers",
+  originPort: "inchen, South Korea",
+  destinationPort: "Port of Algeria",
 };
 
 
