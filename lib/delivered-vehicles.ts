e35af8dece86a1a68ba55331b6ef32516aa84cad
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-31 16:40:36 +0100

    Update delivered-vehicles.ts

--- lib/delivered-vehicles.ts
+++ lib/delivered-vehicles.ts
@@ -1,3 +1,21 @@
+export type DeliveredVehicle = {
+  id: string;
+  vehicleName: string;
+  imageUrl: string;
+  clientCity: string;
+  deliveredMonth: string;
+};
+
+export const DELIVERED_VEHICLES: DeliveredVehicle[] = [
+  {
+    id: "1",
+    vehicleName: "Kia picanto 2023",
+    imageUrl: "/delivered/FullSizeRender.jpeg",
+    clientCity: "biskra",
+    deliveredMonth: "juillet 2026",
+  },
+];
+
 export const DELIVERED_VEHICLES: DeliveredVehicle[] = [
    
    {
