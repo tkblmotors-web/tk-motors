3fc16fbbc5ffe44db2be29541051eec0c3e3b1a5
tkblmotors-web <tkbl.motors@gmail.com>
2026-08-31 17:28:11 +0100

    Create HtmlLangSetter.tsx

---
+++ components/HtmlLangSetter.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+
+export function HtmlLangSetter({ locale }: { locale: string }) {
+  useEffect(() => {
+    document.documentElement.lang = locale;
+    document.documentElement.dir = locale === "ar" ? "rtl" : "ltr";
+  }, [locale]);
+
+  return null;
+}
